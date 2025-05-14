6708820abfeb9ed966cd0d9792cf7583671ef7ec
AdamHELLOO <Adam.Hello@etu.univ-grenoble-alpes.fr>
2025-05-14 10:54:56 +0200

    Modification dynamique de la taille de police

--- parametres/parametres.js
+++ parametres/parametres.js
@@ -1,23 +1,30 @@
-var pFontSize;
+var defaultFontSize = 16; // en pixel
+var pFontSize; // en pourcentage
+
+function updateExemple() {
+    document.getElementById("exemple").style.fontSize = 16*pFontSize/100 + "px"
+}
 
 function fontZoom() {
-    pFontSize = (pFontSize >= 56 ? 56 : pFontSize + 5) 
+    pFontSize = (pFontSize >= 200 ? 200 : pFontSize + 10) 
+    updateExemple()
     console.log("font size : " + pFontSize)
 }
 
 function fontDezoom() {
-    pFontSize = (pFontSize <= 6 ? 6 : pFontSize - 5) 
+    pFontSize = (pFontSize <= 50 ? 50 : pFontSize - 10) 
+    updateExemple()
     console.log("font size : " + pFontSize)
 }
 
 function loadParametres() {
     console.log("load paramètres")
-    try{
-        pFontSize = parseInt(localStorage.getItem("pFontSize"))
-    }catch(error){
+    pFontSize = parseInt(localStorage.getItem("pFontSize"))
+    if(Number.isNaN(pFontSize)){
         console.log("Paramètres par défaut.")
-        pFontSize = 16;
+        pFontSize = 100;
     }
+    document.body.style.fontSize = pFontSize + "%"
 }
 
 function storeParametres() {
