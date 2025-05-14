904e5c6b054aed7be8435d9ea1e9212336615ee1
Lagrange Victoria <lagrangv@im2ag-202-11.u-ga.fr>
2025-05-14 11:07:15 +0200

    regexp mots simples v1

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
 const btnGrp = document.querySelector("#dynamicButtons");
 let userMessage;
-const createChatLi = (message, className) => {
-	const chatLi= document.createElement("li");
-	chatLi.classList.add("chat", className);
-	chatLi.innerHTML = `<p>${message}</p>`;
-	console.log("blavl")
-	return chatLi;
-}
 const randomStrings = [
 	"Je ne comprends pas",
 	"Pouvez-vous m'aider",
@@ -18,6 +11,45 @@ const randomStrings = [
 	"Donnez un exemple",
 	"Recommendations"
 ]
+let keywords= {
+	chute:'chuteSample',
+	brulure:'brulureSample',
+	incendie:'IncendieSample',
+	intoxication:'intoxicationSample',
+	etouffement:'etouffementSample',
+	noyade:'noyadeSample',
+	elongation:'elongationSample',
+	coupe:'coupeSample',
+	morsure:'morsureSample',
+	griffure:'griffureSample',
+	arme:'armeSample',
+}
+
+function searchKeyword(userMsg){
+	let keys= Object.keys(keywords)
+	let re= new RegExp('\\s'+'('+keys.join('|')+')','g')//le mot peut etre conjugué mais pas avoir de prefxe
+	console.log(re)
+	return userMsg.toLowerCase().match(re)
+}
+
+const createChatLi = (message, className) => {
+	const chatLi= document.createElement("li");
+	chatLi.classList.add("chat", className);
+	chatLi.innerHTML = `<p>${message}</p>`;
+	console.log("blavl")
+	return chatLi;
+}
+function handleAnswer(userMsg){
+	console.log("hello")
+	const kw= searchKeyword(userMsg);
+	if (!kw){
+		chatbox.appendChild(createChatLi("No keyword found","incoming"));
+		return;
+	}
+	chatbox.appendChild(createChatLi(kw[0],"incoming"));
+}
+
+
 function pickRandString(){
 	const l = randomStrings.length;
 	const myRandString = randomStrings[Math.floor(Math.random()*l)]
@@ -44,7 +76,6 @@ function updateUI(Usermsg){
 	btnGrp.innerHTML="";
 	chatbox.appendChild(createChatLi(Usermsg,"outgoing"));
 	document.getElementById("msg").value = "";
-	chatbox.appendChild(createChatLi("Je réflechis...","incoming"));
     generateRandomButtons();
 
 }
@@ -53,6 +84,7 @@ chatInput = document.getElementById("msg");
 	userMessage = chatInput.value.trim();
 	if (!userMessage) return;
 	updateUI(userMessage);
+	handleAnswer(userMessage);
 }
 sendChatBtn.addEventListener("click", handleChat);
 
