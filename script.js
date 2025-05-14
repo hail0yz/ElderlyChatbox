34f4dde03be16871a189aab82d9d1c112133af23
Lagrange Victoria <lagrangv@im2ag-202-11.u-ga.fr>
2025-05-14 10:00:45 +0200

    dynqmic buttons

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
-
+const btnGrp = document.querySelector("#dynamicButtons");
 let userMessage;
 const createChatLi = (message, className) => {
 	const chatLi= document.createElement("li");
 	chatLi.classList.add("chat", className);
 	chatLi.innerHTML = `<p>${message}</p>`;
+	console.log("blavl")
 	return chatLi;
 }
-function createButton(mess) {
-  const button= document.createElement("button");
-	button.classList.add("bottom", "btn-grp");
-	button.textContent = mess;
-	return button;
+const randomStrings = [
+	"Je ne comprends pas",
+	"Pouvez-vous m'aider",
+	"Continuez",
+	"Plus d'informations",
+	"Expliquez-moi",
+	"Donnez un exemple",
+	"Recommendations"
+]
+function pickRandString(){
+	const l = randomStrings.length;
+	const myRandString = randomStrings[Math.floor(Math.random()*l)]
+	return myRandString;
 }
-function createRandomString(length) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
+function generateRandomButtons(){
+	btnGrp.innerHTML=""; // Clear previous content
+	const btnNumber = Math.floor(Math.random()*4)+2;
+	for (let i=0; i < btnNumber; i++){
+		const btn = document.createElement("button");
+		//btn.type="button"
+		btn.classList="btn-grp";
+		btn.textContent = pickRandString(); //test
+		btn.onclick=createChatLi;
+		btnGrp.appendChild(btn);
+		console.log("test");
+	}
+	//chatbox.scrollTop=chatbox.scrollHeight;
 }
 function updateUI(Usermsg){
-
+	btnGrp.innerHTML="";
 	chatbox.appendChild(createChatLi(Usermsg,"outgoing"));
 	document.getElementById("msg").value = "";
-	//on genere de maniere random entre 2 et 5 buttons
-	/*k=Math.floor(Math.random() * 4 + 2);
-	for(let i = 0; i < k; i++){
-	 const option = document.createElement("button");
-	    const response = `Option 1`;
-	    option.classList.add("bottom");
-	    option.classList.add("btn-grp");
-	    option.textContent=response;
-	    document.appendChild(option);
-    }*/
-	setTimeout(() => { // message dattente
-		chatbox.appendChild(createChatLi("Je réflechis...","incoming"));
-	},600);
+	chatbox.appendChild(createChatLi("Je réflechis...","incoming"));
+    generateRandomButtons();
 
 }
 const handleChat = () => {
@@ -48,4 +52,5 @@ chatInput = document.getElementById("msg");
 	updateUI(userMessage);
 }
 sendChatBtn.addEventListener("click", handleChat);
+
 
