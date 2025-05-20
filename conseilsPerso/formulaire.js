67bc78653b6b1af0cf8c3836be8f976f22a85309
AdamHELLOO <Adam.Hello@etu.univ-grenoble-alpes.fr>
2025-05-20 13:25:16 +0200

    questions injectées dynamiquement depuis le json dans l'html

--- conseilsPerso/formulaire.js
+++ conseilsPerso/formulaire.js
@@ -1,5 +1,17 @@
-const nbPages = 4;
+var nbPages = 0;
 var currentPage = 1;
+const dictionnaireOrdinaux = [
+    "Première",
+    "Deuxième",
+    "Troisième",
+    "Quatrième",
+    "Cinquième",
+    "Sixième",
+    "Septième",
+    "Huitième",
+    "Neuvième",
+    "Dixième"
+];
 
 var form = document.querySelector("form");
 const form_data = await window.chatbot_app.get_form_data();
@@ -8,8 +20,79 @@ form.addEventListener("submit", submit, false);
 document.getElementById("prevButton").addEventListener("click", prevPage, false);
 document.getElementById("nextButton").addEventListener("click", nextPage, false);
 
+/**
+ * 
+ * @description Fonction d'initialisation du formulaire qui crée les pages et les questions
+ * @param {*} event 
+ * @returns {void}
+ */
 function initForm(event) {
     console.log("initForm");
+
+    nbPages++;
+    let newPage = document.createElement("div");
+    newPage.setAttribute("id", "page" + nbPages);
+    newPage.setAttribute("class", "page");
+    newPage.style.display = "none";
+
+    let pageTitle = document.createElement("h3");
+    pageTitle.textContent = dictionnaireOrdinaux[nbPages - 1] + " page";
+    newPage.appendChild(pageTitle);
+
+    let questions = form_data.questions;
+    console.log(questions.length);
+    for (const q in questions) {
+        let question = questions[q];
+        console.log("for");
+    
+        let questionBox = document.createElement("div");
+        questionBox.setAttribute("class", "question-box");
+    
+        let questionDiv = document.createElement("div");
+        questionDiv.setAttribute("class", "question");
+        questionDiv.textContent = question.question;
+        console.log(question);
+    
+        let answerDiv = document.createElement("div");
+        answerDiv.setAttribute("class", "answers");
+    
+        if (question.type == "text") {
+            console.log("text");
+            let input = document.createElement("input");
+            input.type = "text";
+            input.name = question.name;
+            input.placeholder = question.placeholder || "";
+            answerDiv.appendChild(input);
+        } else if (question.type == "radio") {
+            console.log("radio");
+            for (let i = 0; i < question.options.length; i++) {
+                let label = document.createElement("label");
+                label.textContent = question.options[i];
+    
+                let input = document.createElement("input");
+                input.type = "radio";
+                input.name = question.name;
+                input.value = question.values[i];
+
+                console.log("value : " + input.value);
+    
+                answerDiv.appendChild(label);
+                answerDiv.appendChild(input);
+            }
+        }
+    
+        questionBox.appendChild(questionDiv);
+        questionBox.appendChild(answerDiv);
+        newPage.appendChild(questionBox);
+        console.log(newPage);
+    }
+    document.getElementById("page-list").appendChild(newPage);
+
+
+
+
+
+    console.log("Affichage des pages");
     let page = document.getElementById("page1");
     if (page) {
         page.style.display = "block";
@@ -22,12 +105,23 @@ function initForm(event) {
     }
 }
 
+
+
+
+/***********
+ * Boutons *
+ ***********/
+
+/**
+ * @description Fonction de soumission du formulaire
+ * @param {Event} event
+ * @returns {void}
+ */
 function submit(event) {
     event.preventDefault();
     console.log("submit");
     let data = new FormData(form);
 
-    form_data.username = document.getElementById("username").value;
     for(const entry of data) {
         form_data.answers[entry[0]] = entry[1];
         console.log(JSON.parse(JSON.stringify(form_data)));
@@ -37,6 +131,11 @@ function submit(event) {
     window.chatbot_app.set_form_data(form_data);
 }
 
+/**
+ * @description Passage à la page suivante
+ * @param {Event} event
+ * @returns {void}
+ */
 function nextPage(event) {
     console.log("nextPage");
     if (currentPage < nbPages) {
@@ -48,6 +147,11 @@ function nextPage(event) {
     }
 }
 
+/**
+ * @description Passage à la page précédente
+ * @param {Event} event
+ * @returns {void}
+ */
 function prevPage(event) {
     console.log("prevPage");
     if (currentPage > 1) {
